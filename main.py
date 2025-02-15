4e513bb218d0bb49e84b02596058e099ead24708
ArtemyNeborsky <126957218+ArtemyNeborsky@users.noreply.github.com>
2025-02-15 12:52:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 
 
-url1 = "https://wttr.in/%D0%A7%D0%B5%D1%80%D0%B5%D0%BF%D0%BE%D0%B2%D0%B5%D1%86?nMTq&lang=ru"
-response = requests.get(url1)
-response.raise_for_status()
-print(response.text)
-
-
+url = "https://wttr.in/"
+payload = {
+    "svo": {"lang": "ru", "nMTq": ""},
+    "London": {"lang": "ru", "nMTq": ""},
+    "Череповец": {"lang": "ru", "nMTq": ""}
+}
+for place in payload:
+    response = requests.get(f"{url}{place}", params=payload[place])
+    response.raise_for_status()
+    print(response.text)
